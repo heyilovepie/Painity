6c6b0c6b3022a982cd94a6dbff4d289f873f0ba1
Gillian Ramsay <gillianramsay@shaw.ca>
2015-04-08 17:11:03 -0700

    meow

--- script.js
+++ script.js
@@ -11,13 +11,10 @@ $(function(){
 
   var $picContainer = $("#picture-container");
   var shuffleArray = [];
-  console.log("meow");
-  /*
   for( var i = 0; i < amount; i++){
     shuffleArray[i] = i + 1;
   }
   shuffleArray = shuffle( shuffleArray );
-  console.log("meow");
   var counter = 0;
   for( var i = 0; i < rows; i++ ){
           console.log("outer loop" + i);
@@ -30,6 +27,5 @@ $(function(){
       $div.append($img);
     }
   $picContainer.append($div);
-  */
   }
 });
